400ec60943a6809e3d99ffa04cf980cd17e5421e
davidcoats <david@getsafetycone.com>
2022-05-09 14:42:15 -0700

    Initial commit of source-stub.

---
+++ source/R5T.X0009/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.X0009
+{
+    /// <summary>
+    /// Extensions to Roslyn syntax elements built on R5T.F0012 functionality.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.X0009/Code/Interfaces/Interface1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.X0009
+{
+    public interface Interface1
+    {
+    }
+}
